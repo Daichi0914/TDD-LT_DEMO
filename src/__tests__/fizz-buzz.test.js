23520c2143d1e76b3b22dddcea3f641643736998
daichi.s <daichi.sugawara@nijibox.co.jp>
2024-09-08 19:53:03 +0900

    Red: 3を入力した場合、fizzを返すこと

--- src/__tests__/fizz-buzz.test.js
+++ src/__tests__/fizz-buzz.test.js
@@ -3,3 +3,7 @@ const fizzBuzz = require('../js/fizz.buzz')
 it('入力した数字を返すこと', () => {
   expect(fizzBuzz(1)).toBe(1);
 });
+
+it('3を入力した場合、fizzを返すこと', () => {
+  expect(fizzBuzz(3)).toBe('fizz');
+});
