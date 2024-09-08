35bfd4e43168eea3a008725a3e441572a555c300
daichi.s <daichi.sugawara@nijibox.co.jp>
2024-09-08 19:50:07 +0900

    Red: 入力した数字を返すこと

--- src/__tests__/fizz-buzz.test.js
+++ src/__tests__/fizz-buzz.test.js
@@ -3,3 +3,9 @@ const fizzBuzz = require('../js/fizz.buzz')
 it('1を返すこと', () => {
   expect(fizzBuzz()).toBe(1);
 });
+
+it('入力した数字を返すこと', () => {
+  expect(fizzBuzz(1)).toBe(1);
+  expect(fizzBuzz(2)).toBe(2);
+  expect(fizzBuzz(3)).toBe(3);
+});
