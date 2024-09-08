308b47c3d21583c3bb77d4a05430902b9e89f463
daichi.s <daichi.sugawara@nijibox.co.jp>
2024-09-08 19:57:12 +0900

    Ref: 3の倍数を入力した場合、fizzを返すこと

--- src/__tests__/fizz-buzz.test.js
+++ src/__tests__/fizz-buzz.test.js
@@ -4,10 +4,6 @@ it('入力した数字を返すこと', () => {
   expect(fizzBuzz(1)).toBe(1);
 });
 
-it('3を入力した場合、fizzを返すこと', () => {
-  expect(fizzBuzz(3)).toBe('fizz');
-});
-
 it('3の倍数を入力した場合、fizzを返すこと', () => {
   expect(fizzBuzz(3)).toBe('fizz');
   expect(fizzBuzz(6)).toBe('fizz');
